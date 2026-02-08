ce771b79b3ef6ad12e915c3eb0447ceb4a19db5d
Bahamut <leander.volprich@web.de>
2026-02-08 04:30:49 +0100

    Refactor deck management and sorting logic; improve CSS formatting for deck components

--- src/Components/Deck/index.js
+++ src/Components/Deck/index.js
@@ -1,47 +1,60 @@
-import React from 'react'
-import {useDrop} from 'react-dnd'
-import shortid from 'shortid'
-import Card from '../Card'
-import {useDispatch, useSelector} from 'react-redux'
-import Button from '@material-ui/core/Button';
-import './deck.css'
+import React from "react";
+import { useDrop } from "react-dnd";
+import shortid from "shortid";
+import Card from "../Card";
+import { useDispatch, useSelector } from "react-redux";
+import Button from "@material-ui/core/Button";
+import "./deck.css";
 
 const Deck = () => {
-    const dispatch = useDispatch()
-    let main_deck = useSelector(state => state.deck.main)
-    let extra_deck = useSelector(state => state.deck.extra)
-    const [, drop] = useDrop({
-        accept: "Card",
-        drop: item => {
-            dispatch({type: "ADD_CARD_TO_DECK", payload: item.card})
-        }
-    })
+  const dispatch = useDispatch();
+  let main_deck = useSelector((state) => state.deck.main);
+  let extra_deck = useSelector((state) => state.deck.extra);
+  const [, drop] = useDrop({
+    accept: "Card",
+    drop: (item) => {
+      dispatch({ type: "ADD_CARD_TO_DECK", payload: item.card });
+    },
+  });
 
-    const handleSort = () => {
-        dispatch({ type: 'SORT_DECK' });
-    };
+  const handleSort = () => {
+    dispatch({ type: "SORT_DECK" });
+  };
 
-    
-       return (
-           <div className="deck-container" ref={drop}>
-               <div style={{ marginBottom: '10px' }}>
-                   <Button variant="contained" color="primary" onClick={handleSort}>
-                       Sort Deck
-                   </Button>
-               </div>
-                <div className="deck">
-                    <div className="main-deck-container">
-                        {main_deck.map( (card, index) => <Card cardInfo={card} key={shortid.generate()} isDraggable={false} index={index}/>)}
-                    </div>
-                    {(main_deck.length > 0 && extra_deck.length > 0) && <div className="deck-separator" />}
-                    <div className="extra-deck-container">
-                        {extra_deck.map( (card, index) => <Card cardInfo={card} key={shortid.generate()} isDraggable={false} index={index}/>)}
-                    </div>
-                </div>
-           </div>
-       )
-   
-}
+  return (
+    <div className="deck-container" ref={drop}>
+      <div style={{ marginBottom: "10px" }}>
+        <Button variant="contained" color="primary" onClick={handleSort}>
+          Sort Deck
+        </Button>
+      </div>
+      <div className="deck">
+        <div className="main-deck-container">
+          {main_deck.map((card, index) => (
+            <Card
+              cardInfo={card}
+              key={shortid.generate()}
+              isDraggable={false}
+              index={index}
+            />
+          ))}
+        </div>
+        {main_deck.length > 0 && extra_deck.length > 0 && (
+          <div className="deck-separator" />
+        )}
+        <div className="extra-deck-container">
+          {extra_deck.map((card, index) => (
+            <Card
+              cardInfo={card}
+              key={shortid.generate()}
+              isDraggable={false}
+              index={index}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Deck
+export default Deck;
--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,79 +1,105 @@
-import { sortMainDeck, sortExtraDeck, belongsToExtraDeck, sortAllCards } from '../../utils/deckSorter'
-
+import {
+  sortMainDeck,
+  sortExtraDeck,
+  belongsToExtraDeck,
+  sortAllCards,
+} from "../../utils/deckSorter";
 
 const reducer = (state, action) => {
-    let {payload, type, index} = action
-    switch (type) {
-        case 'UPDATE_LISTER':
-            return {
-                ...state,
-                lister: sortAllCards(payload)
-            }
+  let { payload, type, index } = action;
+  switch (type) {
+    case "UPDATE_LISTER":
+      return {
+        ...state,
+        lister: sortAllCards(payload),
+      };
 
-        case 'UPDATE_LISTER_ITEMS':
-            return {
-                ...state,
-                lister: [...state.lister, ...sortAllCards(payload)]
-            }
-        
-        case 'ADD_CARD_TO_DECK':
-            if ( belongsToExtraDeck(payload.type)){
-                    return {
-                        ...state,
-                        deck: {main: [...state.deck.main], extra:[...state.deck.extra, payload]}
-                    }
-                } else {
-                        return {
-                            ...state,
-                            deck: {main: [...state.deck.main, payload], extra:[...state.deck.extra]}
-                        }
-                }
+    case "UPDATE_LISTER_ITEMS":
+      return {
+        ...state,
+        lister: [...state.lister, ...sortAllCards(payload)],
+      };
 
-        case 'REMOVE_CARD':
-            if ( belongsToExtraDeck(payload.type)){
-                return {
-                    ...state,
-                    deck: {main: [...state.deck.main], extra: [...state.deck.extra.slice(0,index).concat(state.deck.extra.slice(index+1))]}
-                }
-            } else {
-                return {
-                    ...state,
-                    deck: {main: [...state.deck.main.slice(0,index).concat(state.deck.main.slice(index+1))], extra: [...state.deck.extra]}
-                }
-            }
-        
-        case 'ERASE_DECK':
-            return {
-                ...state, deck: {main: [], extra: []}
-            }
+    case "ADD_CARD_TO_DECK":
+      if (belongsToExtraDeck(payload.type)) {
+        return {
+          ...state,
+          deck: {
+            main: [...state.deck.main],
+            extra: [...state.deck.extra, payload],
+          },
+        };
+      } else {
+        return {
+          ...state,
+          deck: {
+            main: [...state.deck.main, payload],
+            extra: [...state.deck.extra],
+          },
+        };
+      }
 
-        case 'SET_LOADING_STATE':
-            return {
-                ...state, isLoading: payload
-            }
-        
-        case 'SET_HAS_MORE_ITEMS_TO_LOAD':
-            return {
-                ...state, hasMoreItemsToLoad: payload
-            }
-        
-        case 'SET_NEXT_PAGE_TO_LOAD':
-            return {
-                ...state,
-                nextPageToLoad: payload
-            }
-        
-        case 'SORT_DECK':
-            return {
-                ...state,
-                deck: {
-                    main: sortMainDeck(state.deck.main),
-                    extra: sortExtraDeck(state.deck.extra)
-                }
-            }
+    case "REMOVE_CARD":
+      if (belongsToExtraDeck(payload.type)) {
+        return {
+          ...state,
+          deck: {
+            main: [...state.deck.main],
+            extra: [
+              ...state.deck.extra
+                .slice(0, index)
+                .concat(state.deck.extra.slice(index + 1)),
+            ],
+          },
+        };
+      } else {
+        return {
+          ...state,
+          deck: {
+            main: [
+              ...state.deck.main
+                .slice(0, index)
+                .concat(state.deck.main.slice(index + 1)),
+            ],
+            extra: [...state.deck.extra],
+          },
+        };
+      }
 
-    }
-    return state
-}
+    case "ERASE_DECK":
+      return {
+        ...state,
+        deck: { main: [], extra: [] },
+      };
 
-export default reducer
+    case "SET_LOADING_STATE":
+      return {
+        ...state,
+        isLoading: payload,
+      };
+
+    case "SET_HAS_MORE_ITEMS_TO_LOAD":
+      return {
+        ...state,
+        hasMoreItemsToLoad: payload,
+      };
+
+    case "SET_NEXT_PAGE_TO_LOAD":
+      return {
+        ...state,
+        nextPageToLoad: payload,
+      };
+
+    case "SORT_DECK":
+      return {
+        ...state,
+        deck: {
+          main: sortMainDeck(state.deck.main),
+          extra: sortExtraDeck(state.deck.extra),
+        },
+      };
+  }
+  return state;
+};
+
+export default reducer;
